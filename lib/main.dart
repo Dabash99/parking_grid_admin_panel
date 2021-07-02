8006b8fb1d2fcbae958ff0d760e2523991848d5e
Dabash99 <61636920+Dabash99@users.noreply.github.com>
2021-07-03 01:49:01 +0200

    Finish Admin App ...!

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Parking Admin Panel',
       theme: lightTheme,
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
